72de66a72df4bdcb5147800b6a71804b8495307a
Ryan Johnson <137436926+RyanSR71@users.noreply.github.com>
2025-08-21 15:35:27 -0700

    Update prior.py

--- GWCorrect/ppE/prior.py
+++ GWCorrect/ppE/prior.py
@@ -21,7 +21,7 @@ def match(signal,data,duration,**kwargs):
     
     signal_match = np.sqrt(bilby.gw.utils.matched_filter_snr(signal,signal,PSDs,4))
     data_match = np.sqrt(bilby.gw.utils.matched_filter_snr(data,data,PSDs,4))
-    normalized_match = np.abs(bilby.gw.utils.matched_filter_snr(signal,data,PSDs,4)/(signal_match*data_match))
+    normalized_match = np.real(bilby.gw.utils.matched_filter_snr(signal,data,PSDs,4)/(signal_match*data_match))
     
     return normalized_match
 
